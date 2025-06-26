67c8d09fcbd248803aa0859366f388ba2f207a16
NASHE\shote <nachogamallolafon123@gmail.com>
2025-06-26 07:46:31 +0200

    First program, with simples comments

---
+++ FirstSteps/firstProgram.cpp
@@ -0,0 +1,8 @@
+#include <iostream> //Directiva, la cual nos incluye las bibliotecas basicas de C++. 
+using namespace std; //Declaracion de espacio de nombre. 
+
+void main(){
+
+    cout << "Hola mundo." << endl; //cout se usa para imprimir cosas por consola, pondremos el texto entre los signos << y endl, realiza un salto de linea. 
+
+}
